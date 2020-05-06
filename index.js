ea5f280089d5e69ff788914cad99af8980266ef0
unknown <loskotaklp@gmail.com>
2020-05-07 00:58:35 +0200

    second commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var T = new Twit({
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  // strictSSL:            true,     // optional - requires SSL certificates to be valid.
+  // strictSSL:            true,     // optional - requires SSL certificates to be valid. //asdasdsad
 })
 
 var stream = T.stream('statuses/sample')
